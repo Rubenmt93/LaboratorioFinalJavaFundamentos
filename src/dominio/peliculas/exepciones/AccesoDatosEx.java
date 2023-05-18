b437860f423fd5f04d66f178c6a9e5979db98abc
LAPTOP-BBEJBMMH\ruben <rubenmontes330@gmail.com>
2023-05-18 13:20:39 +0100

    Prueba2

--- src/dominio/peliculas/exepciones/AccesoDatosEx.java
+++ src/dominio/peliculas/exepciones/AccesoDatosEx.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package dominio.peliculas.exepciones;
 
-/**
- *
- * @author ruben
- */
-public class AccesoDatosEx {
+public class AccesoDatosEx extends Exception{
+
+    public AccesoDatosEx(String message) {
+        super(message);
+    }
     
 }
